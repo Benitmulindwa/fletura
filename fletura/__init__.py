e48290279ff5855aa150aa0293827b15bfcae5d2
Benitmulindwa <mulindwabenit@gmail.com>
2024-06-16 15:58:31 +0200

    controlled rating

--- fletura/__init__.py
+++ fletura/__init__.py
@@ -1,1 +1,2 @@
 from .paper import Paper
+from .rating import Rating
--- fletura/rating.py
+++ fletura/rating.py
@@ -6,19 +6,25 @@ from dataclasses import dataclass
 class RatingSize:
     small = 0.5
     medium = 1
-    large = 2
+    large = 1.5
+    extralarge = 2
 
 
 class Rating(Row):
     def __init__(
-        self, max_value: int = 5, rating_value: float = 0.0, rating_icon: str = None
+        self,
+        max_value: int = 5,
+        rating_value: float = 0.0,
+        rating_icon: str = None,
+        size: str = "large",
+        on_hover_color=colors.YELLOW,
     ):
         super().__init__()
 
         self.max_value: int = max_value
         self.rating_value: float = rating_value
         self.rating_icon: str = rating_icon
-
+        self.size = size
         self.controls = [
             Container(
                 IconButton(
@@ -29,20 +35,40 @@ class Rating(Row):
                     on_click=self.on_icon_clicked,
                     content=Icon(icons.ACCOUNT_TREE),
                     style=ButtonStyle(
+                        color={
+                            MaterialState.HOVERED: on_hover_color,
+                        },
                         overlay_color={
                             MaterialState.HOVERED: colors.TRANSPARENT,
                             MaterialState.FOCUSED: colors.BLUE,
                             MaterialState.DEFAULT: colors.WHITE,
                         },
+                        padding=padding.all(0),
                     ),
                     scale=1.0,
-                    padding=padding.all(0),
-                )
+                    # padding=padding.all(0),
+                ),
+                margin=margin.all(-7),
+                on_hover=self.icon_hovered,
             )
             for _ in range(self.max_value)
         ]
+        if size == "small":
+            self.scale = RatingSize.small
+        elif size == "medium":
+            self.scale = RatingSize.medium
+        elif size == "extralarge":
+            self.scale = RatingSize.extralarge
+        else:
+            self.scale = RatingSize.large
+
         self.spacing = 0
         self.alignment = "center"
+
+    def icon_hovered(self, e):
+        # e.control.content.selected = True if e.data == "true" else False
+        e.control.content.scale = 1.5 if e.data == "true" else 1.0
+        e.control.content.update()
 
     def on_icon_clicked(self, e):
         rating_list = e.control.parent.parent.controls
@@ -56,7 +82,7 @@ def main(page: Page):
     page.vertical_alignment = "center"
     page.alignment = "center"
     page.add(
-        Rating(max_value=1),
+        Rating(max_value=5),
     )
 
 
