75808972bf3abbb8833021f8b00ced2a51e46711
Benitmulindwa <mulindwabenit@gmail.com>
2024-06-21 21:55:49 +0200

    Timeline: Completed

--- fletura/timeline.py
+++ fletura/timeline.py
@@ -1,17 +1,57 @@
 from flet import *
 
 
+class TimelineDot(Container):
+    def __init__(
+        self,
+        icon: str = None,
+        icon_size: int = 0,
+        icon_color: str = colors.WHITE,
+        dot_size: int = 5,
+        dot_border_radius: int = 50,
+        border: Border = border.all(3, color="white"),
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.content = (
+            Icon(
+                kwargs.get(icon),
+                size=kwargs.get(icon_size),
+                color=kwargs.get(icon_color, colors.BLUE),
+            )
+            if icon != None
+            else None
+        )
+        self.alignment = alignment.center
+        self.padding = padding.all(dot_size)
+        self.border = border
+        self.border_radius = dot_border_radius
+
+
+class TimelineSeparator(Container):
+    def __init__(
+        self, color: str = colors.WHITE, width: int = 2, height: int = 50, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.content = Container(
+            width=width,
+            height=height,
+            bgcolor=color,
+            alignment=alignment.center,
+        )
+
+
 class TimelineEvent(Container):
     def __init__(
         self,
         title: str,
+        dot_props: dict = {},
+        separator_props: dict = {},
         title_style: TextStyle = None,
         description: str = None,
         description_style: TextStyle = None,
         timestamp: str = None,
         timestamp_style: TextStyle = None,
-        icon: str = icons.EVENT,
-        icon_size: int = 20,
         content_position: int = 8,
         **kwargs
     ):
@@ -19,32 +59,21 @@ class TimelineEvent(Container):
         self.title = title
         self.description = description
         self.timestamp = timestamp
-        self.icon = icon
-        self.icon_size = icon_size
+
         self.content_position = content_position
         self.title_style = title_style
         self.description_style = description_style
         self.timestamp_style = timestamp_style
 
+        self.dot_props = dot_props
+        self.separator_props = separator_props
+
         self.content = Row(
             [
                 Column(
                     [
-                        Container(
-                            # Icon(self.icon, size=self.icon_size, color=colors.BLUE),
-                            alignment=alignment.center,
-                            padding=padding.all(5),
-                            border=border.all(3, color="white"),
-                            border_radius=10,
-                        ),
-                        Container(
-                            width=2,
-                            height=50,
-                            bgcolor="white",
-                            alignment=alignment.center,
-                            margin=margin.only(top=5),
-                            #
-                        ),
+                        TimelineDot(**self.dot_props),
+                        TimelineSeparator(**self.separator_props),
                     ],
                     alignment="center",
                     horizontal_alignment="center",
@@ -106,25 +135,66 @@ def main(page: Page):
     events = [
         {
             "title": "Event 1",
-            # "description": "This is the description for event 1.",
-            # "timestamp": "2023-01-01 10:00 AM",
+            "description": "This is the description for event 1.",
+            "timestamp": "2023-01-01 10:00 AM",
             # "icon": icons.EVENT,
             # "icon_size": 30,
+            "dot_props": {
+                # "border_color": "red",
+                # "border_radius": 2,
+                # "dot_border_radius": 0,
+                "border": border.all(2, color="blue"),
+            },
             "content_position": 0,
+            "separator_props": {
+                # "color": colors.BLUE,
+                # "width": 3,
+                "height": 70,
+                "margin": margin.only(top=5),
+            },
+            "content_position": 25,
         },
         {
             "title": "Event 2",
             "description": "This is the description for event 2.",
             "timestamp": "2023-02-01 12:00 PM",
-            "icon": icons.CALENDAR_TODAY,
-            "icon_size": 25,
+            # "icon": icons.CALENDAR_TODAY,
+            # "icon_size": 25,
+            "dot_props": {
+                # "border_color": "red",
+                # "border_radius": 2,
+                # "dot_border_radius": 0,
+                "border": border.all(2, color="red"),
+            },
+            "content_position": 0,
+            "separator_props": {
+                # "color": colors.BLUE,
+                # "width": 3,
+                "height": 70,
+                "margin": margin.only(top=5),
+            },
+            "content_position": 25,
         },
         {
             "title": "Event 3",
             "description": "This is the description for event 3.",
             "timestamp": "2023-03-01 02:00 PM",
-            "icon": icons.DATE_RANGE,
-            "icon_size": 20,
+            # "icon": icons.DATE_RANGE,
+            # "icon_size": 20,
+            "dot_props": {
+                # "border_color": "red",
+                # "border_radius": 2,
+                # "dot_border_radius": 0,
+                "border": border.all(2, color="green"),
+            },
+            "content_position": 0,
+            "separator_props": {
+                # "color": colors.BLUE,
+                # "width": 3,
+                "height": 70,
+                "margin": margin.only(top=5),
+            },
+            "content_position": 20,
         },
     ]
 
